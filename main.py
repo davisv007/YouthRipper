2b53e8ea47b63a213d812d59b101048ce6e94102
davisv007 <davisvberea@gmail.com>
2018-02-10 12:03:12 -0500

    added trash code

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ ydl_opts = {
 
 def main():
 
-    # url ="https://www.youtube.com/watch?v=4o5baMYWdtQ"
-    url = "https://www.youtube.com/watch?v=-tT32VTll5M"
+    url ="https://www.youtube.com/watch?v=4o5baMYWdtQ"
+    # url = "https://www.youtube.com/watch?v=-tT32VTll5M"
     # url = "https://www.youtube.com/watch?v=FUXX55WqYZs"
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(url, download=False)
@@ -60,4 +60,35 @@ def main():
     #     ydl.download([url])
 
 
+
 main()
+
+
+###################################################################################
+#code that works with audio and can be repurposed
+# from pydub import AudioSegment
+# from os import listdir, stat, mkdir, getcwd
+# from os.path import isfile, join
+#
+#
+# def is_wav_file(cwd, fname):
+#     if len(fname) < 4:
+#         return False
+#     else:
+#         return isfile(join(cwd, fname)) and fname[-3:] == 'wav'
+#
+#
+# def main():
+#     op = 'output'
+#     try:
+#         stat(op)
+#     except:
+#         mkdir(op)
+#
+#     cwd = getcwd()
+#     wavfiles = [f for f in listdir(cwd) if is_wav_file(cwd, f)]
+#     for fname in wavfiles:
+#         wav = AudioSegment.from_wav(fname)
+#         wav.export('{0}/{1}/{2}mp3'.format(cwd, op, fname[:-3]), format="mp3", bitrate='320k')
+#
+#
