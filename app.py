4d317a162e732340d8f115e89b3ed39e8bae438e
davisv007 <davisvberea@gmail.com>
2018-07-11 00:16:44 -0400

    formatting

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
-from flask import Flask, request, render_template, url_for, send_from_directory
-from flask_wtf import Form
+from flask import Flask, render_template, send_from_directory
+from flask_wtf import FlaskForm
 from wtforms import StringField
+
 from meat import download
-
-
-
-
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'stuff'
 app.config['UPLOAD_FOLDER'] = 'albumzips/'
 
+
 @app.route('/', methods=['GET', 'POST'])
 def divide():
-
-    class UrlLink(Form):
-        webLink = StringField("Album Name")
-
+    class UrlLink(FlaskForm):
+        webLink = StringField("Album Name:")
 
     form = UrlLink()
     result = None
@@ -27,9 +23,11 @@ def divide():
 
     return render_template('divide.html', result=result, form=form)
 
+
 @app.route('/albumzips/<path:filename>')
 def download_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],filename, as_attachment=True)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+
 
 if __name__ == '__main__':
     app.run(port=5000)
