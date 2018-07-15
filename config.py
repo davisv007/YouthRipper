63a2017e88843e15a738f30464a3100716c237de
davisv7 <davisv@berea.edu>
2018-07-15 13:42:50 -0400

    got it working again, next to remove need for selenium

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
-from os import getcwd, path,mkdir
+from os import getcwd, path
 
 import pylast
 
 API_KEY = "a0472e3ba14a8c6b2d373f25f7214f47"
 API_SECRET = "e3706cbf68860e14e3da9bf6968b66c4"
 network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET)
-ALBUMSLOCATION = path.join(getcwd(), 'output')
+FULLALBUMSLOC = path.join(getcwd(), 'output')
 ZIPLOCATION = path.join(getcwd(), 'albumzips')
+SPLITALBUMSLOC = path.join(getcwd(), 'albums')
 
 
 # Determines when video is done converting
--- meat.py
+++ meat.py
@@ -1,20 +1,39 @@
 import datetime  # date and time stamp
+import pathlib
 import re  # regular expression module used for searching patterns
+import shutil
+import time
 from urllib.parse import quote
 
 import requests
 import wikipedia
 import youtube_dl  # downloads youtube via links
 from bs4 import BeautifulSoup
-from mutagen.id3 import ID3, APIC, error
+from mutagen.id3 import ID3, APIC
 from mutagen.mp3 import EasyMP3 as MP3
-from pydub import silence
+from pydub import silence, AudioSegment
+from selenium.webdriver import Chrome, ChromeOptions
 
 from config import *
 
+from pytube import YouTube
+
+
+def get_info(url):
+    chrome_options = ChromeOptions()
+    chrome_options.add_argument('--headless')
+    driver = Chrome(chrome_options=chrome_options)
+    driver.get(url)
+    time.sleep(2)
+    duration = driver.find_element_by_xpath(
+        "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch/div[2]/div[1]/div[1]/div/div[21]/div[2]/div[1]/div/span[3]").text
+    description = driver.find_element_by_xpath('//*[@id="description"]').get_attribute("innerText")
+    driver.quit()
+    return duration, description
+
 
 def fix_filename(string):
-    valid_file_name = re.sub('[^\w_.)( -]', '', string.replace(' ', '_'))
+    return re.sub('[^\w_.)( -]', '', string.replace(' ', '_'))
 
 
 def song_lengths_to_timesstamps(x):
@@ -45,12 +64,25 @@ def get_cover_art(artist, album):
 
 
 def get_youtube_time_stamps(description):
-    pattern = r'[0-2]{2}:[0-9]{2}:[0-9]{2}|[0-9]{2}:[0-9]{2}|[0-9]:[0-9]{2}'  # Time stamp length #Hour:Minute:Seconds or Minute:Second or Second:Second
+    pattern = r'\d{0,2}:?\d{1,2}:\d{2}'  # Time stamp length #Hour:Minute:Seconds or Minute:Second or Second:Second
     p = re.compile(pattern=pattern)  # compiles pattern
     timestamps = p.findall(description)
     # if sum(timestamps[0].split(':')) == {0}:  #trying to find out if the timestamp is all zeroes
     #     timestamps = timestamps[1:]
-    return timestamps
+    if timestamps == []:
+        return None
+    else:
+        return timestamps
+
+
+def get_youtube_tracks(description):
+    pattern = r'(?:\d{0,2}:?\d{1,2}:\d{2})(.*)'  # Time stamp length #Hour:Minute:Seconds or Minute:Second or Second:Second
+    p = re.compile(pattern=pattern)  # compiles pattern
+    tracklist = p.findall(description)
+    # from each track, remove the timestamp
+    pattern = r'\d{2}:\d{2}:\d{2}|\d{2}:\d{2}|\d{1}:\d{2}|\d{1}:\d{2}:\d{2}'
+    tracklist = [re.sub(pattern, '', track) for track in tracklist]
+    return tracklist
 
 
 def timestamps_to_milliseconds(timestamps):
@@ -68,15 +100,12 @@ def timestamps_to_milliseconds(timestamps):
     return newtimes
 
 
-def split_tracks_using_milliseconds(audio, artist, album, timelist, tracks):
-    for i in range(len(timelist) - 1):
+def split_tracks_using_milliseconds(audio, artist, album, timelist, track_dict):
+    for i, ele in enumerate(list(track_dict.keys())):
         track = audio[timelist[i]:timelist[i + 1]]
-        track.export(out_f=ALBUMSLOCATION + "{0}\{1}\{1}\{2}.mp3".format(artist, album, artist + ' - ' + tracks[i]),
+        track.export(out_f=path.join(SPLITALBUMSLOC, artist, album, track_dict[ele] + ".mp3"),
                      format='mp3',
                      bitrate="320k")
-    track = audio[timelist[-1]:]
-    track.export(out_f=ALBUMSLOCATION + "{0}\{1}\{1}\{2}.mp3".format(artist, album, artist + ' - ' + tracks[-1]),
-                 format='mp3', bitrate="320k")
 
 
 def split_tracks_by_silence(audio, artist, album, timelist, tracks):
@@ -93,11 +122,11 @@ def split_tracks_by_silence(audio, artist, album, timelist, tracks):
 
     for i in range(len(new_timelist) - 1):
         track = audio[new_timelist[i]:new_timelist[i + 1]]
-        track.export(out_f=ALBUMSLOCATION + "{0}\{1}\{1}\{2}.mp3".format(artist, album, artist + ' - ' + tracks[i]),
+        track.export(out_f=FULLALBUMSLOC + "{0}\{1}\{1}\{2}.mp3".format(artist, album, artist + ' - ' + tracks[i]),
                      format='mp3',
                      bitrate="320k")
     track = audio[new_timelist[-1]:]
-    track.export(out_f=ALBUMSLOCATION + "{0}\{1}\{1}\{2}.mp3".format(artist, album, artist + ' - ' + tracks[-1]),
+    track.export(out_f=FULLALBUMSLOC + "{0}\{1}\{1}\{2}.mp3".format(artist, album, artist + ' - ' + tracks[-1]),
                  format='mp3', bitrate="320k")
 
 
@@ -141,84 +170,98 @@ def get_song_lengths_from_wiki(albumName, albumArtist, maxlen=None):
         return timeStamps
 
 
-def save_art(artwork_link, location, album):
-    with open('{0}{1}\\{2}.png'.format(location, album, album), 'wb') as f:
+def save_art(artwork_link, location):
+    with open(path.join(location, 'cover.png'), 'wb') as f:
         f.write(requests.get(artwork_link).content)
         f.close()
 
 
-def update_tags(location, artist, album, tracklist):
+def update_tags(artist, album, track_dict):
     # edit the ID3 tag to add the title, artist, artwork, date, and genre
-    for index, title in enumerate(tracklist):
+    for index, title in enumerate(list(track_dict.keys())):
         # print(location+"{}.mp3".format(title))
         # break
-        audiofile = MP3(location + "{0}\{1} - {2}.mp3".format(album, artist, title))
-        try:
-            audiofile.add_tags()
-        except error:
-            pass
+        audiofile = MP3(path.join(SPLITALBUMSLOC, artist, album, track_dict[title] + '.mp3'))
+        # try:
+        #     audiofile.add_tags()
+        # except error:
+        #     pass
         # print(audiofile.keys())
         audiofile['album'] = album
         audiofile['artist'] = artist
         audiofile['tracknumber'] = str(index + 1)
         audiofile['title'] = title
         audiofile.save(v2_version=3)
-        audiofile = MP3(location + "{0}\{1} - {2}.mp3".format(album, artist, title), ID3=ID3)
+        audiofile = MP3(path.join(SPLITALBUMSLOC, artist, album, track_dict[title] + '.mp3'), ID3=ID3)
         audiofile.tags.add(
             APIC(
                 encoding=3,  # 3 is for utf-8
                 mime='image/png',  # image/jpeg or image/png
                 type=3,  # 3 is for the cover image
                 desc=u'Cover',
-                data=open("{0}{1}\{1}.png".format(location, album), 'rb').read()
+                data=open(path.join(SPLITALBUMSLOC, artist, album, 'cover.png'), 'rb').read()
             )
         )
+        # audiofile.save()
         audiofile.save(v2_version=3)
 
 
 def download(album, artist):
+    album_filename = fix_filename(album)
+    pathlib.Path(path.join(FULLALBUMSLOC, artist, album_filename)).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(path.join(SPLITALBUMSLOC, artist, album_filename)).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(path.join(ZIPLOCATION, artist, album_filename)).mkdir(parents=True, exist_ok=True)
+
     url = search_youtube(album)
+    print('Url:{} Artist:{} Album:{}.'.format(url, artist, album))
+
+    ydl_opts['outtmpl'] = path.join(FULLALBUMSLOC, artist, album_filename, '%(title)s.mp3')
+
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(url, download=False)
-        # description = info_dict['description']
-        # tracks = get_tracks_from_lastfm(artist, album)
-        # cover_art = get_cover_art(artist, album)
-        print('Url:{} Artist:{} Album:{}.'.format(url, artist, artist))
-    #     pathlib.Path(path.join(ALBUMSLOCATION, artist, album)).mkdir(parents=True, exist_ok=True)
-    #     pathlib.Path(path.join(ALBUMSLOCATION, artist, album, album)).mkdir(parents=True, exist_ok=True)
-    #     save_art(cover_art, path.join(ALBUMSLOCATION, artist), album)
-    #     ydl_opts['outtmpl'] = path.join('output', artist, album, '%(title)s.mp3')
-    #
-    # # find track times (if present)
-    # # timestamps = get_youtube_time_stamps(description)
-    # # print(timestamps)
-    # # if timestamps == []:
-    # songlengths = get_song_lengths_from_wiki(album, artist)
-    # timestamps = song_lengths_to_timesstamps(songlengths)
-    # print(len(timestamps))
-    # print(len(tracks))
-    #
-    # # download album
-    # with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    #     ydl.download([url])
-    #
-    # # load audio into memory
-    # audio_location = path.join(ALBUMSLOCATION, artist, album, title, '.mp3')
-    # albumaudio = AudioSegment.from_file(audio_location)
-    #
-    # # convert list of time stamps into milliseconds
-    # millitimes = timestamps_to_milliseconds(timestamps)
-    # split_tracks_using_milliseconds(albumaudio, artist, album, millitimes, tracks)
-    # # split_tracks_by_silence(albumaudio, artist, album,millitimes, tracks)
-    #
-    # # add tags
-    # location = path.join(ALBUMSLOCATION, artist)
-    # update_tags(location, artist, album, tracks)
-    #
-    # # make
-    # shutil.make_archive(path.join(ZIPLOCATION, album), 'zip', path.join(ALBUMSLOCATION, artist, album, album))
-    # return path.join('albumzips', album, '.zip')
+        title = info_dict['title']
+        # ydl.download([url])
+    duration, description = get_info(url)
+    # print(info_dict)
+    # print(title)
+    # print(description)
+    # print(duration)
 
+    # find track times (if present)
+    timestamps = get_youtube_time_stamps(description)
+    timestamps.append(duration)
+    # if timestamps == []:
+    #     songlengths = get_song_lengths_from_wiki(album, artist)
+    #     timestamps = song_lengths_to_timesstamps(songlengths)
+    # tracks = get_tracks_from_lastfm(artist, album)
 
-if __name__ == '__main__':
-    download(input("enter the name of the album you want: "))
+    tracks = get_youtube_tracks(description)
+    track_filenames = [fix_filename(track) for track in tracks]
+    track_dict = {track: filename for track, filename in zip(tracks, track_filenames)}
+
+    # print(tracks)
+    # print(timestamps)
+    # print(path.exists(path.join(FULLALBUMSLOC, artist, album_filename, title+'.mp3')))
+
+    cover_art = get_cover_art(artist, album)
+    save_art(cover_art, path.join(SPLITALBUMSLOC, artist, album))
+
+    # load audio into memory
+    audio_location = path.join(FULLALBUMSLOC, artist, album, title + '.mp3')
+    albumaudio = AudioSegment.from_file(audio_location)
+
+    # convert list of time stamps into milliseconds
+    millitimes = timestamps_to_milliseconds(timestamps)
+    split_tracks_using_milliseconds(albumaudio, artist, album, millitimes, track_dict)
+    # split_tracks_by_silence(albumaudio, artist, album,millitimes, tracks)
+
+    # add tags
+    # location = path.join(FULLALBUMSLOC, artist)
+    update_tags(artist, album, track_dict)
+
+    # make
+    shutil.make_archive(path.join(ZIPLOCATION, artist, album), 'zip', path.join(SPLITALBUMSLOC, artist, album))
+    return path.join('albumzips', artist, album + '.zip')
+
+# if __name__ == '__main__':
+# #     download(input("enter the name of the album you want: "))
